79e5a3ce47ab0381a5cf342862bb573a78b85644
Subramanian <vishwanath79@outlook.com>
2016-11-28 21:57:33 -0800

    cron version

--- dailyreddit.py
+++ dailyreddit.py
@@ -6,7 +6,10 @@ import pytz
 subbredditnames = ['learnpython', 'Python', 'bigdata']
 fmt = "%Y-%m-%d %H:%M:%S %Z%z"
 user_agent = ('LearnReddit 0.1(by /u/ingvay7)')
-r = praw.Reddit(user_agent=user_agent)
+r = praw.Reddit(client_id='gKKxVJc9vJgvrg',
+                     client_secret='	yYSe-u68S_0zzst1_0BnwP43_K8',
+                    user_agent= user_agent,
+                     username='ingvay7')
 n = 20
 allposts = []
 
@@ -18,7 +21,6 @@ def get_posts():
         titles = "\n ############# " + str(sub) + " ############# \n"
         allposts.append(titles)
         for count, submission in enumerate(subreddit.get_top_from_day(limit=n)):
-            # print(count, submission.title, ':', submission.url, submission.score, datetime.utcfromtimestamp(submission.created_utc).replace(tzinfo=pytz.utc).strftime(fmt)))
             posts = count, submission.title, ':', submission.url, submission.score, datetime.utcfromtimestamp(
                 submission.created_utc).replace(tzinfo=pytz.utc).strftime(fmt)
             allposts.append(str(posts).replace(':', ' '))
@@ -31,8 +33,7 @@ def get_posts():
 # now_time = datetime.utcfromtimestamp(submission.created_utc).replace(tzinfo=pytz.utc)
 # print(now_time.strftime(fmt))
 
-
-
+# 0 17  * * * sh reddit.sh |mail -E -r "email" -s "$(echo -e "Daily Reddit\nContent-Type: text/html")"  "email"
 
 
 if __name__ == '__main__':
